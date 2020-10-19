bcffb1632173aa41ed7820b8841c8466b52978cd
Sergei Demchuk <vinci.ru@mail.ru>
2020-10-19 23:36:22 +0300

    fix: fix tabs

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
     let arr = str.split("");
-    for(let i = 0; i < arr.length; i++){
-        for (let b = 0; b < bracketsConfig.length; b++) {
-            if (bracketsConfig[b][0] == arr[i] && arr[i+1] ==bracketsConfig[b][1]){
-                arr.splice(i,2);
-            i = 0;
-            b = -1;  
-            }                  
-        }
+    for (let i = 0; i < arr.length; i++) {
+      for (let b = 0; b < bracketsConfig.length; b++) {
+        if(bracketsConfig[b][0] == arr[i] && bracketsConfig[b][1] == arr[i+1]){
+          arr.splice(i,2);
+          i = 0;
+          b = -1;
+        }      
+      }    
     }
-  if( arr.length == 0){
-    return true;
-  }else{
-    return false;
-  }
+
+    if(arr.length == 0){
+      return true;    
+    }else{
+      return false;
+    }
 };
