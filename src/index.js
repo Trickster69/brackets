38ca2c2e37d4e3638cc76ef8102d664618992c2e
Sergei Demchuk <vinci.ru@mail.ru>
2020-10-19 23:05:38 +0300

    feat: add brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let arr = str.split("");
+    for(let i = 0; i < arr.length; i++){
+        for (let b = 0; b < bracketsConfig.length; b++) {
+            if (bracketsConfig[b][0] == arr[i] && arr[i+1] ==bracketsConfig[b][1]){
+                arr.splice(i,2);
+            i = 0;
+            b = -1;  
+            }                  
+        }
+    }
+  if( arr.length == 0){
+    return true;
+  }else{
+    return false;
+  }
+};
